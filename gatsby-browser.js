76dc8146037feb5057600a5040fce49fe6bdc70e
Rodrigo Moreira <hola@piducan.dev>
2021-06-25 10:38:56 -0400

    fix undefined when disableScrollUpdate not present

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 // https://www.joshwcomeau.com/gatsby/the-worlds-sneakiest-route-change/
 
 export const shouldUpdateScroll = ({ routerProps }) => {
-  const { disableScrollUpdate } = routerProps.location.state
-  return !disableScrollUpdate
+  return !routerProps.location.state?.disableScrollUpdate
 }
